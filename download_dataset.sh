c5cfbca47e290d17f2ae77582da56ee5b6f266d3
Adrian Cheung <acheung46@gatech.edu>
2025-04-06 12:37:56 -0400

    create script to download data

---
+++ download_dataset.sh
@@ -0,0 +1,6 @@
+folder_name="amazon-dataset"
+
+mkdir $folder_name
+curl -L -o $folder_name/amazon-fine-food-reviews.zip https://www.kaggle.com/api/v1/datasets/download/snap/amazon-fine-food-reviews
+unzip $folder_name/amazon-fine-food-reviews.zip -d $folder_name
+rm $folder_name/amazon-fine-food-reviews.zip
